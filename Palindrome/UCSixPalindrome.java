3d985e7a72a5d5d4492d60b02fe49b4be0432fe6
Shrivatsa Guru <shrivatsaindra77@gmail.com>
2026-03-05 10:43:30 +0530

    [Shrivatsa Guru]: Implemented palindrome checking using Queue and Stack

--- Palindrome/UCSixPalindrome.java
+++ Palindrome/UCSixPalindrome.java
@@ -1,3 +1,30 @@
+/**
+ * ===============================================================
+ * MAIN CLASS – UseCase6PalindromeCheckerApp
+ * ===============================================================
+ *
+ * Use Case 6: Queue + Stack Fairness Check
+ *
+ * Description:
+ * This class demonstrates palindrome validation using
+ * two different data structures:
+ *
+ *  - Queue (FIFO – First In First Out)
+ *  - Stack (LIFO – Last In First Out)
+ *
+ * Characters are inserted into both structures and then
+ * compared by removing from the front of the queue and
+ * the top of the stack.
+ *
+ * If all characters match, the input string is confirmed
+ * as a palindrome.
+ *
+ * This use case helps understand how FIFO and LIFO
+ * behaviors can be combined for symmetric comparison.
+ *
+ * @author Developer
+ * @version 6.0
+ */
 import java.util.*;
 
 public class UCSixPalindrome
